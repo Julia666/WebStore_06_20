91b0540344435906e2d72584331d66441f930a65
Julia666 <zazerkalie@yandex.ru>
2020-06-26 14:20:21 +0300

    Отображение списка сотрудников и их карточек

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,15 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly List<Employee> _Employees = new List<Employee>
         {
-            return View(); // если в () ничего не указано, то ищется представление с именем действия, 
-                            // иначе в () указываем какое конкретно представление хотим отобразить View("Index")
+            new Employee
+            {
+                Id = 1,
+                SurName = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Age = 39
+            },
+
+            new Employee
+            {
+                Id = 2,
+                SurName = "Петров",
+                Name = "Пётр",
+                Patronymic = "Петрович",
+                Age = 27
+            },
+
+            new Employee
+            {
+                Id = 3,
+                SurName = "Сидоров",
+                Name = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 18
+            },
+        };
+
+        public IActionResult Index() => View();  // если в () ничего не указано, то ищется представление с именем действия,
+                                                 // иначе в () указываем какое конкретно представление хотим отобразить View("Index")
+
+
+        // адрес, по которому наш сайт выдаст наших сотрудников и отобразит их в виде html-страницы
+        // метод, в котором плучаем данные из бизнес-логики, а дальше передадим их на представление 
+        // (вызовем метод View и передадим ему модель - перечисление сотрудников)
+        public IActionResult Employees()
+        {
+            ViewBag.Title = "123";// - динамический объект
+            ViewData["TestValue"] = "Value - test"; // - словарь
+
+            var employees = _Employees;
+            return View(employees);
+        } 
+
+        public IActionResult EmployeeInfo(int id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id); // извлекает 1й элемент с каким-то совпадением по какому-то критерию
+            if (employee == null)
+                return NotFound(); // код ошибки 404
+
+            return View(employee);
         }
     }
 }
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -12,5 +12,9 @@ namespace WebStore.Models
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+        public string FullName
+        {
+            get { return this.SurName + " " + this.Name + " " + this.Patronymic; }
+        }
     }
 }
