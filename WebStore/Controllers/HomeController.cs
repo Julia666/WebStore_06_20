afd13d9e8c122785cd0b4523052f771e407848a1
Julia666 <zazerkalie@yandex.ru>
2020-06-30 13:57:15 +0300

    Заготовки представлений для основных страниц дизайнерского макета

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -13,6 +13,17 @@ namespace WebStore.Controllers
 
         public IActionResult Index() => View();  // если в () ничего не указано, то ищется представление с именем действия,
                                                  // иначе в () указываем какое конкретно представление хотим отобразить View("Index")
-     
+
+
+        public IActionResult Blogs() => View();
+        public IActionResult BlogSingle() => View(); 
+        
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
+
     }
 }
