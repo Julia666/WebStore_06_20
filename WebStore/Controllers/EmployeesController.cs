5ea069d6a283059e5cbf133ef2f2605512291c4e
Julia666 <zazerkalie@yandex.ru>
2020-07-06 12:57:50 +0300

    Переносим тестовые данные в отдельное место

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -10,46 +11,18 @@ namespace WebStore.Controllers
     // [Route("Users")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                SurName = "Иванов",
-                Name = "Иван",
-                Patronymic = "Иванович",
-                Age = 39
-            },
-
-            new Employee
-            {
-                Id = 2,
-                SurName = "Петров",
-                Name = "Пётр",
-                Patronymic = "Петрович",
-                Age = 27
-            },
-
-            new Employee
-            {
-                Id = 3,
-                SurName = "Сидоров",
-                Name = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 18
-            },
-        };
+       
 
         // [Route("All")]
-        public IActionResult Index()
+        public IActionResult Ind
         {
-            return View(_Employees);
+            return View(TestData.Employees);
         }
 
         // [Route("User-{id}")]
         public IActionResult Details(int id)
         {
-            var employee = _Employees.FirstOrDefault(e => e.Id == id); // извлекает 1й элемент с каким-то совпадением по какому-то критерию
+            var employee = TestData.Employees.FirstOrDefault(e => e.Id == id); // извлекает 1й элемент с каким-то совпадением по какому-то критерию
             if (employee == null)
                 return NotFound(); // код ошибки 404
 
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; } = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                SurName = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Age = 39
+            },
+
+            new Employee
+            {
+                Id = 2,
+                SurName = "Петров",
+                Name = "Пётр",
+                Patronymic = "Петрович",
+                Age = 27
+            },
+
+            new Employee
+            {
+                Id = 3,
+                SurName = "Сидоров",
+                Name = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 18
+            },
+        };
+    }
+}
