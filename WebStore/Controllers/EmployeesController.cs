9712f17c8b9c141f8251d899de2d07ffa86995a0
Julia666 <zazerkalie@yandex.ru>
2020-07-06 21:24:44 +0300

    Заготовка методов для редактирования сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -4,29 +4,63 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     // [Route("Users")]
     public class EmployeesController : Controller
     {
-       
+        private readonly IEmployeesData _EmployeesData;
+        public EmployeesController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+        
 
         // [Route("All")]
         public IActionResult Index()
         {
-            return View(TestData.Employees);
+            return View(_EmployeesData.Get()); // получение всех сотрудников
         }
 
         // [Route("User-{id}")]
-        public IActionResult Details(int id)
+        public IActionResult Details(int id) // посмотреть конкретного сотрудника по его id
         {
-            var employee = TestData.Employees.FirstOrDefault(e => e.Id == id); // извлекает 1й элемент с каким-то совпадением по какому-то критерию
+            var employee = _EmployeesData.GetById(id); // извлекает 1й элемент с каким-то совпадением по какому-то критерию
             if (employee == null)
                 return NotFound(); // код ошибки 404
 
             return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id is null) // если идентификатор не передан
+              return View(new EmployeesViewModel()); // то создаем нового сотрудника и передаем пустую модель на представление
+
+            if (id < 0)
+                return BadRequest();
+
+            var employee = _EmployeesData.GetById((int)id); // извлекаем из сервиса редактируемого сотрудника
+            if (employee is null) //если сервис не нашел сотрудника
+                return NotFound();
+            // если сотрудник найден, создаем вьюмодель и пердаем на нее информацию
+            return View(new EmployeesViewModel
+            {
+                Id = employee.Id, // передача вьюмодели на представление
+                FirstName = employee.Name,
+                LastName = employee.SurName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+
+            }); ; ; 
+        }
+
+        [HttpPost] // обрабатывает ответ формы
+        public IActionResult Edit(EmployeesViewModel Model)
+        {
+            return RedirectToAction(nameof(Index)); // перенаправляет на метод Index, чтобы отобразить всех сотрудников
         }
     }
 
---
+++ WebStore/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    // передача информации в представление или из представления
+    // когда нам возвращается из формы. когда мы формируем представление и вызываем его с помощью метода View,
+    // мы должны собрать всю необходимую информацию, чтобы это представление смогло сформироваться, упаковать ее в какой-то контейнер
+    // вот этим контейнером и является модельпредставления
+    public class EmployeesViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
