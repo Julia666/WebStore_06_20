9a3bfda9ad362f2e2d03d26d1ed703f1ace0a50f
Julia666 <zazerkalie@yandex.ru>
2020-07-10 13:19:45 +0300

    Визуальные компоненты списка секций и брендов

---
+++ WebStore/ViewModels/BrandViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebStoreDomain.Entities.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
