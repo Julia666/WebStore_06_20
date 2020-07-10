9e684b813f8f9bd866cda2649c5115967506d3eb
Julia666 <zazerkalie@yandex.ru>
2020-07-10 14:11:37 +0300

    Реализация выгрузки товаров из сервиса каталога товаров с возможностью фильтрации

--- WebStore/Infrastructure/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreDomain;
 using WebStoreDomain.Entities;
 
 namespace WebStore.Infrastructure.Interfaces
@@ -11,5 +12,6 @@ namespace WebStore.Infrastructure.Interfaces
         IEnumerable<Section> GetSections();
         IEnumerable<Brand> GetBrands();
 
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null); // выгрузить все товары по указанному фильтру
     }
 }
--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
+using WebStoreDomain;
 using WebStoreDomain.Entities;
 
 namespace WebStore.Infrastructure.Services
@@ -12,5 +13,18 @@ namespace WebStore.Infrastructure.Services
     {
         public IEnumerable<Section> GetSections() => TestData.Sections;
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            var query = TestData.Products;  // берем все товары как перечисления, после чего накладываем фильтры
+            if (Filter?.SectionId != null)
+                query = query.Where(product => product.SectionId == Filter.SectionId);
+
+            if (Filter?.BrandId != null)
+                query = query.Where(product => product.BrandId == Filter.BrandId);
+
+            return query;
+
+        }
     }
 }
