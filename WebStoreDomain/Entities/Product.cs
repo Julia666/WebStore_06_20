9f5aed2646b9a54ad3108e99102be72121a50789
Julia666 <zazerkalie@yandex.ru>
2020-07-10 13:49:55 +0300

    Товар + фильтр для его выгрузки из сервиса каталога товаров

---
+++ WebStoreDomain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStoreDomain.Entities.Base;
+using WebStoreDomain.Entities.Base.Interfaces;
+
+namespace WebStoreDomain.Entities
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int SectionId { get; set; }
+
+        public int? BrandId { get; set; } // опционально номер бренда
+
+        public string ImageUrl { get; set; } // ссылка на картинку
+
+        public decimal Price { get; set; }
+    }
+}
