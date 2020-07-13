d8255fad33e79ff89090736059f3243640bcf58a
Julia666 <zazerkalie@yandex.ru>
2020-07-14 00:02:20 +0300

    Контекст БД готов

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using WebStoreDomain.Entities;
 
 namespace WebStore.DAL.Context
 {
     public class WebStoreDB: DbContext
     {
+        // определяем набор таблиц, с которыми хотим работать
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
         public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base (Options) {}
     }
 }
