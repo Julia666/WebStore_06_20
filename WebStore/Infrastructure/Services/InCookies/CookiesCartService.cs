be6e56d5c7910c63161ee95aa9775481d2b3d67e
Julia666 <zazerkalie@yandex.ru>
2020-07-21 12:54:38 +0300

    Осноовная логика сервиса корзины

---
+++ WebStore/Infrastructure/Services/InCookies/CookiesCartService.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Infrastructure.Services.InCookies
+{
+    public class CookiesCartService : ICartService
+    {
+        private readonly IProductData _ProductData;
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        private readonly string _CartName;
+
+       private Cart Cart 
+        { 
+            get
+            {
+                var context = _HttpContextAccessor.HttpContext;  // берем контекст http-запроса и из него извлекаем cookies
+                var cookies = context.Response.Cookies;
+                var cart_cookies = context.Request.Cookies.[_CartName]; // пытаемся найти саму cookies, которая отвечает за хранение корзины
+                if(cart_cookies is null)  // если cookies вообще нет, то создаем новую корзину и сериализуем её
+                {
+                    var cart = new Cart();
+                    cookies.Append(_CartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCookies(cookies, cart_cookies); // если cookies была, то её необходимо подменить
+                return JsonConvert.DeserializeObject<Cart>(cart_cookies); // десериализуем её
+            }
+            set => ReplaceCookies(_HttpContextAccessor.HttpContext.Response.Cookies, JsonConvert.SerializeObject(value)); 
+        }
+
+        private void ReplaceCookies(IResponseCookies cookies, string cookie)
+        {
+            cookies.Delete(_CartName);
+            cookies.Append(_CartName, cookie);
+        }
+
+        public CookiesCartService(IProductData ProductData, IHttpContextAccessor HttpContextAccessor) // HttpContextAccessor извлекает информацию из контекста http-запроса
+        {
+            _ProductData = ProductData;
+            _HttpContextAccessor = HttpContextAccessor;
+
+            var user = HttpContextAccessor.HttpContext.User;
+            var user_name = user.Identity.IsAuthenticated ? $"[{user.Identity.Name}]": null;
+            _CartName = $"WebStore.Cart{user_name}"; // если пользователь не залогинился, то название cookies будет WebStore.Cart, в противном случае добавляем в [] его имя
+        }
+
+        public void AddToCart(int id) 
+        {
+            var cart = Cart; // извлекаем корзину из cookies
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id); // пытаемся найти в корзине товар 
+            if (item is null)   // если товара не было найдено  в корзине, то надо добавить новый
+                cart.Items.Add(new CartItem { ProductId = id, Quantity = 1 });
+            else
+                item.Quantity++;
+
+            Cart = cart; // корзина обратно сериализуется
+
+        }
+
+        public void DecrementFromCart(int id)
+        {
+            var cart = Cart;
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+                return;
+            if (item.Quantity > 0)
+                item.Quantity--;
+
+            if (item.Quantity == 0)
+                cart.Items.Remove(item);
+
+            Cart = cart;
+        }
+
+        public void RemoveFromCart(int id) 
+        {
+            var cart = Cart;
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+                return;
+
+            cart.Items.Remove(item);
+
+            Cart = cart;
+        }
+
+        public void Clear() 
+        {
+            var cart = Cart;
+
+            cart.Items.Clear();
+
+            Cart = cart;
+
+        }
+
+        public CartViewModel TransformFromCart() { throw new NotImplementedException(); }
+
+    }
+}
