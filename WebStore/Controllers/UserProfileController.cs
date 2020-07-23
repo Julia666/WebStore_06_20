fdda9e75a1a62884b4858e124acbb6d808c97ebc
Julia666 <zazerkalie@yandex.ru>
2020-07-23 17:15:29 +0300

    Заготовка профиля пользователя

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
