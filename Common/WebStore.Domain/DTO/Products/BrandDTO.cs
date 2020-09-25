023d3972fe74975cf9395fc3fae86b0ddbf65fe0
Julia666 <zazerkalie@yandex.ru>
2020-09-25 15:49:38 +0300

    DTO - модели для представления товара

---
+++ Common/WebStore.Domain/DTO/Products/BrandDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.DTO.Products
+{
+    class BrandDTO : INamedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+
+
+}
