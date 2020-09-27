e32506030cd9a3cb17c1b5b1d7724be9cffa19b7
Julia666 <zazerkalie@yandex.ru>
2020-09-27 23:14:22 +0300

    Клиент для WebAPI каталога товаров

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -1,5 +1,4 @@
 using System;
-using JetBrains.ReSharper.Psi.JavaScript.Impl.ControlFlow.Inspections.ValueAnalysis.SimpleAnalysis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebStore.Clients.Employees;
+using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
@@ -92,7 +92,7 @@ namespace WebStore
             //services.AddScoped<IEmployeesData, InMemoryEmployeesData>(); // в коллекцию сервисов добавляем сервис, регистрируем его
             services.AddScoped<IEmployeesData, EmployeesClient>();
             //services.AddScoped<IProductData, InMemoryProductData>();
-            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IProductData, ProductsClient>();
             services.AddScoped<ICartService, CookiesCartService>();
             services.AddScoped<IOrderService, SqlOrderService>();
 
