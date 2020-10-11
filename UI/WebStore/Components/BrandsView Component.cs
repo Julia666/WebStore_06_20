41154c9bfb345dd078a6d480bd707ddc6072142f
Julia666 <zazerkalie@yandex.ru>
2020-10-11 23:23:20 +0300

    Подсветка выбранной секции в боковой панели

--- UI/WebStore/Components/BrandsView Component.cs
+++ UI/WebStore/Components/BrandsView Component.cs
@@ -13,7 +13,7 @@ namespace WebStore.Components
         private readonly IProductData _ProductData;
         public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData; 
         
-        public IViewComponentResult Invoke() => View(GetBrands());
+        public IViewComponentResult Invoke(string BrandId) => View(GetBrands());
 
         private IEnumerable<BrandViewModel> GetBrands() =>
             _ProductData.GetBrands()
