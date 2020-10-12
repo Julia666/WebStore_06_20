f6c97deab0a1a81bd3c7a9ae540e47aedebc3e9c
Julia666 <zazerkalie@yandex.ru>
2020-10-12 04:15:57 +0300

    Подготовка визуального компонента хлебных крошек

---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BreadCrumbsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbViewModel();
+
+
+            if (int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var product_id))
+            {
+                var product = _ProductData.GetProductById(product_id);
+                if (product != null)
+                    model.Product = product.Name;
+            }
+
+            return View(model);
+        }
+    }
+}
