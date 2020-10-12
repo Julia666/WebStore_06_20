d2713a20242b50790b396b94950854b1daf1341f
Julia666 <zazerkalie@yandex.ru>
2020-10-12 05:20:42 +0300

    Использование информации по бренду и секции при формировании представлений хлебных крошек

--- UI/WebStore/Components/BreadCrumbsViewComponent.cs
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
+using WebStore.Services.Mapping;
 
 namespace WebStore.Components
 {
@@ -18,6 +19,11 @@ namespace WebStore.Components
         {
             var model = new BreadCrumbViewModel();
 
+            if (int.TryParse(Request.Query["SectionId"], out var section_id))
+                model.Section = _ProductData.GetSectionById(section_id).FromDTO();
+
+            if (int.TryParse(Request.Query["BrandId"], out var brand_id))
+                model.Brand = _ProductData.GetBrandById(brand_id).FromDTO();
 
             if (int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var product_id))
             {
