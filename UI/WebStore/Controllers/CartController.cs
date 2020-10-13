9635dd1987e35155b4128c06cf75374e3f0d6bff
Julia666 <zazerkalie@yandex.ru>
2020-10-13 07:24:10 +0300

    Добавлена логика api-корзины в основной контроллер корзины (в целях резервирования)

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -77,5 +77,38 @@ namespace WebStore.Controllers
             ViewBag.OrderId = id;
             return View();
         }
+
+        #region API
+
+        public IActionResult GetCartView() => ViewComponent("Cart");
+
+        public IActionResult AddToCartAPI(int id)
+        {
+            _CartService.AddToCart(id);
+            return Json(new { id, message = $"Товар с id:{id} был добавлен в корзину" });
+            //return new JsonResult(new { id, message = $"Товар с id:{id} был добавлен в корзину" });
+        }
+
+        public IActionResult DecrementFromCartAPI(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return Ok();
+        }
+
+        public IActionResult RemoveFromCartAPI(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return Ok();
+        }
+
+
+        public IActionResult ClearAPI()
+        {
+            _CartService.Clear();
+            return Ok();
+        }
+
+        #endregion
+
     }
 }
