0c20c96c388877de849c5e4210ef069fdc30adef
Julia666 <zazerkalie@yandex.ru>
2020-10-14 17:05:14 +0300

    Доработка фильтра каталога товаров для постраничого разбиения

--- Common/WebStore.Domain/ProductFilter.cs
+++ Common/WebStore.Domain/ProductFilter.cs
@@ -6,10 +6,10 @@ namespace WebStore.Domain
 {
     public class ProductFilter // возможность выдачи не всех товаров, а только удовлетворяющих критерию поиска 
     {
-        public int? SectionId { get; set; } // задаем номер секции и номер бренда опционально- 
-                                            // если указан номер секции, то должны быть выгружены все товары, у которых есть этот номер
-        public int? BrandId { get; set; } 
-
+        public int? SectionId { get; set; } // задаем номер секции и номер бренда опционально- // если указан номер секции, то должны быть выгружены все товары, у которых есть этот номер
+        public int? BrandId { get; set; }
         public int[] Ids { get; set; } // идентификаторы товаров
+        public int Page { get; set; }
+        public int? PageSize { get; set; }
     }
 }
